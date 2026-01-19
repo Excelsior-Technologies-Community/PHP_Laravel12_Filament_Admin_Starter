858285cf5d2a4dffc699c70cb845c005aec23aa7
hardikpanchal001 <139863292+hardikpanchal001@users.noreply.github.com>
2026-01-19 15:16:32 +0530

    Initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Filament\Panel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, HasRoles;
+
+    /**
+     * Mass assignable attributes
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * Hidden attributes
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Attribute casting
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    /**
+     * ✅ Filament Admin Access Control
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true; // allow all users for now
+    }
+}
